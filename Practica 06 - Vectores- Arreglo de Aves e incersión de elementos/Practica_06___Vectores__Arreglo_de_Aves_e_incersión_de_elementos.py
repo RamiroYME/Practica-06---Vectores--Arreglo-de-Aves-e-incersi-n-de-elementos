42beea380a511f913ab5cb53347294ba8eae899b
Ramiro Yahir Merida Espinoza <rymerida21@gmail.com>
2024-09-02 19:18:09 -0600

    Agregar archivos de proyecto.

---
+++ Practica 06 - Vectores- Arreglo de Aves e incersión de elementos/Practica_06___Vectores__Arreglo_de_Aves_e_incersión_de_elementos.py
@@ -0,0 +1,19 @@
+# Declara una lista de cadenas
+aves=["Loro gris","Paloma de diamantes","Coctel"]
+print("Los valores de la lista antes de insertar:")
+# |tera sobre la lista para imprimir los valores
+for ave in aves:
+    print(ave,end="")
+
+print("\n")
+
+#Agrega trs valores al final de la lista utilizando el metodo append()
+aves.append("Mayna")
+aves.append("Periquitos")
+aves.append("Cacatua")
+print("Los valores de la lista despues de insertar:")
+# |tera sobre la lista para imprimir los valores 
+for ave in aves:
+    print(ave,end="")
+
+print("\n")
